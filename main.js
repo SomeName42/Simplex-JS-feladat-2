faba5a5c9e7855350f703899f6f39c250864fd10
SomeName42 <>
2024-11-29 10:59:41 +0100

    Added dragging from time table to time table

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ let subjects = [
 let cell_size = 30;
 
 
-let subject_rows = {}
+let subjects_rows = {}
 
 
 function minutes_to_string(t) {
@@ -22,26 +22,78 @@ function minutes_to_string(t) {
 }
 
 
-function on_drag_start(e) {
-    e.dataTransfer.setData("text", e.originalTarget.innerText)
+function add_subject(s_row_i, s_col_i, subject) {
+    let subject_rows = subjects_rows[subject];
+
+    if(subject_rows === undefined) {
+        return;
+    }
+
+    let table = $("#time_table_body")[0];
+
+    if(table.children.length < s_row_i + subject_rows) {
+        return;
+    }
+
+    let cell = table.children[s_row_i].children[s_col_i];
+
+    if(cell.textContent != "") {
+        return;
+    }
+
+    cell.textContent = subject;
+    cell.setAttribute("rowspan", subject_rows);
+    cell.setAttribute("class", "draggable");
+    cell.setAttribute("draggable", "true");
+    cell.setAttribute("ondragstart", "on_time_table_drag_start(event)");
+
+    for(let row_i = s_row_i + 1; row_i < s_row_i + subject_rows; ++row_i) {
+        table.children[row_i].children[s_col_i].setAttribute("class", "d-none");
+    }
 }
+
+
+function remove_subject(s_row_i, s_col_i) {
+    let table = $("#time_table_body")[0];
+    let cell = table.children[s_row_i].children[s_col_i];
+    let subject_rows = subjects_rows[cell.textContent];
+
+    cell.textContent = "";
+    cell.setAttribute("rowspan", "");
+    cell.setAttribute("class", "");
+    cell.setAttribute("draggable", "");
+    cell.setAttribute("ondragstart", "");
+
+    for(let row_i = s_row_i + 1; row_i < s_row_i + subject_rows; ++row_i) {
+        table.children[row_i].children[s_col_i].setAttribute("class", "");
+    }
+}
+
+
+function on_subjects_drag_start(e) {
+    e.dataTransfer.setData("text", e.originalTarget.innerText);
+}
+
+
+function on_time_table_drag_start(e) {
+    e.dataTransfer.setData("text", e.originalTarget.innerText);
+
+    let s_row_i = Number(e.target.parentElement.attributes.row_i.value);
+    let s_col_i = Number(e.target.attributes.col_i.value) + 1;
+
+    remove_subject(s_row_i, s_col_i);
+}
+
 
 
 function on_drop(e) {
     e.preventDefault();
-    if(e.target.textContent == "") {
-        let subject = e.dataTransfer.getData("text");
-        e.target.textContent = subject;
-        e.target.setAttribute("rowspan", subject_rows[subject])
 
-        let t_col_i = Number(e.target.attributes.col_i.value) + 1;
-        let t_row_i = Number(e.target.parentElement.attributes.row_i.value);
-        let table = e.target.parentElement.parentElement;
+    let s_row_i = Number(e.target.parentElement.attributes.row_i.value);
+    let s_col_i = Number(e.target.attributes.col_i.value) + 1;
+    let subject = e.dataTransfer.getData("text");
 
-        for(let row_i = t_row_i + 1; row_i < t_row_i + subject_rows[subject]; ++row_i) {
-            table.children[row_i].children[t_col_i].setAttribute("class", "d-none")
-        }
-    }
+    add_subject(s_row_i, s_col_i, subject);
 }
 
 
@@ -64,7 +116,7 @@ $(document).ready(function() {
     }
 
     subjects.forEach((subject) => {
-        subject_rows[subject[0]] = subject[1] / cell_size;
-        $("#subjects_table_body").append(`<tr class = "text-center align-middle"><td>${minutes_to_string(subject[1])}</td><td draggable="true" class = "movable" ondragstart="on_drag_start(event)">${subject[0]}</td></tr>`)
+        subjects_rows[subject[0]] = subject[1] / cell_size;
+        $("#subjects_table_body").append(`<tr class = "text-center align-middle"><td>${minutes_to_string(subject[1])}</td><td draggable="true" class = "draggable" ondragstart="on_subjects_drag_start(event)">${subject[0]}</td></tr>`)
     })
 });
